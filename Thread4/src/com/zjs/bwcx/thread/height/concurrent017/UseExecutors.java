2ec59a1f004621e62dd73787f8811f83414d6a5d
shizhijie <18232529363@163.com>
2019-02-11 12:42:20 +0800

    Thread4

---
+++ Thread4/src/com/zjs/bwcx/thread/height/concurrent017/UseExecutors.java
@@ -0,0 +1,14 @@
+package com.zjs.bwcx.thread.height.concurrent017;
+
+public class UseExecutors {
+
+	public static void main(String[] args) {
+		
+		//ExecutorService pool = Executors.newSingleThreadExecutor()
+		
+		//cache fixed single
+		
+		
+		
+	}
+}
