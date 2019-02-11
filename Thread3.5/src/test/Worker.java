6caa7103d7a8caa1252f2acf17509160b4bfd981
shizhijie <18232529363@163.com>
2019-02-11 22:19:17 +0800

    Thread3.5

--- Thread3.5/src/test/Worker.java
+++ Thread3.5/src/test/Worker.java
@@ -22,23 +22,12 @@ public class Worker implements Runnable {
 			Task input = this.workQueue.poll();
 			if (null == input) break;
 			//真正的去做业务处理
-			Object output = handle(input);
+			Object output = MyWorker.handle(input);
 			this.resultMap.put(Integer.toString(input.getId()), output);
 		}
 	}
-
-	private Object handle(Task input) {
-		Object output = null;
-		
-		try {
-			//表示处理task任务的耗时，可能是数据的加工，也可能是操所数据库。。。
-			Thread.sleep(500);
-			
-			output = input.getPrice();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return output;
+	
+	public static Object handle(Task input) {
+		return null;
 	}
 }
