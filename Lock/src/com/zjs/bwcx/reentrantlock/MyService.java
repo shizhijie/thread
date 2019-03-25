aab11afa52d64452be5d33d749ec13775187de8e
shizhijie <2272684702@qq.com>
2019-03-25 15:55:40 +0800

    commit lock java.project

---
+++ Lock/src/com/zjs/bwcx/reentrantlock/MyService.java
@@ -0,0 +1,15 @@
+package com.zjs.bwcx.reentrantlock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyService {
+	private Lock lock = new ReentrantLock();
+	public void testMethod() {
+		lock.lock();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("ThreadName="+Thread.currentThread().getName()+("\t"+(i+1)));
+		}
+		lock.unlock();
+	}
+}
